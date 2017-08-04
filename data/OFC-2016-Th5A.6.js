69b82babca5a4841798a2dcc60e3b7ada7313499
Santiago Echeverri Chacón <santiag77e@gmail.com>
2017-08-04 15:20:03 +0200

    Initialization
    Added files and created a gh-pages branch

---
+++ data/OFC-2016-Th5A.6.js
@@ -0,0 +1,539 @@
+var data = {
+  "config": {
+    "id": "36a3b0b5-bad0-4a04-b79b-441c7cef77db",
+    "label": "BetterBibTeX JSON",
+    "release": "1.6.100",
+    "preferences": {
+      "asciiBibLaTeX": true,
+      "asciiBibTeX": true,
+      "attachmentsNoMetadata": false,
+      "autoAbbrevStyle": "http://www.zotero.org/styles/cell",
+      "autoAbbrev": false,
+      "autoExport": "idle",
+      "autoExportIdleWait": 10,
+      "cacheFlushInterval": 5,
+      "confirmCacheResetSize": 1000,
+      "itemObserverDelay": 100,
+      "caching": true,
+      "citeCommand": "cite",
+      "citekeyFormat": "[auth:lower][year]",
+      "citekeyFold": true,
+      "debug": false,
+      "DOIandURL": "both",
+      "bibtexURL": "url",
+      "csquotes": "",
+      "keyConflictPolicy": "keep",
+      "langID": "babel",
+      "pinCitekeys": "manual",
+      "preserveBibTeXVariables": false,
+      "rawImports": false,
+      "scanCitekeys": false,
+      "showCitekeys": false,
+      "showItemIDs": false,
+      "skipFields": "",
+      "skipWords": "a,ab,aboard,about,above,across,after,against,al,along,amid,among,an,and,anti,around,as,at,before,behind,below,beneath,beside,besides,between,beyond,but,by,d,da,das,de,del,dell,dello,dei,degli,della,dell,delle,dem,den,der,des,despite,die,do,down,du,during,ein,eine,einem,einen,einer,eines,el,en,et,except,for,from,gli,i,il,in,inside,into,is,l,la,las,le,les,like,lo,los,near,nor,of,off,on,onto,or,over,past,per,plus,round,save,since,so,some,sur,than,the,through,to,toward,towards,un,una,unas,under,underneath,une,unlike,uno,unos,until,up,upon,versus,via,von,while,with,within,without,yet,zu,zum",
+      "tests": "",
+      "warnBulkModify": 10,
+      "postscript": "",
+      "jabrefGroups": 4,
+      "defaultDateParserLocale": "",
+      "bibtexParticleNoOp": false,
+      "biblatexExtendedNameFormat": true,
+      "biblatexExtendedDateFormat": false,
+      "quickCopyMode": "latex",
+      "quickCopyPandocBrackets": false,
+      "jurismPreferredLanguage": "zh-alalc97",
+      "ZotFile": true,
+      "qualityReport": false,
+      "suppressTitleCase": false
+    },
+    "options": {
+      "exportPath": "C:\\Users\\santiagoe\\Dropbox\\Data visualization course\\Assignment2\\Relational_visualization\\data",
+      "exportFilename": "OFC-2016-Th5A.6.json",
+      "exportFileData": false,
+      "exportNotes": false
+    }
+  },
+  "collections": [],
+  "items": [
+    {
+      "itemType": "conferencePaper",
+      "tags": [
+        "Fiber characterization",
+        "Fiber design and fabrication",
+        "Fiber optics and optical communications"
+      ],
+      "relations": {},
+      "citations": "15",
+      "url": "http://www.osapublishing.org/abstract.cfm?URI=OFC-2016-Th5A.1",
+      "publisher": "Optical Society of America",
+      "pages": "Th5A.1",
+      "date": "2016",
+      "extra": "bibtex: Hayashi:16",
+      "DOI": "10.1364/OFC.2016.Th5A.1",
+      "abstractNote": "A 125-textmum-cladding coupled four-core fiber realized 0.158-dB/km attenuation at 1550 nm and 6.1-ps/km1/2 spatial mode dispersion over C-band, both of which are the lowest ever reported among optical fibers for the space-division multiplexed transmission.",
+      "title": "125-textmum-cladding Coupled Multi-core Fiber with Ultra-low Loss of 0.158 dB/km and Record-low Spatial Mode Dispersion of 6.1 ps/km1/2",
+      "proceedingsTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "creators": [
+        {
+          "creatorType": "author",
+          "lastName": "Hayashi",
+          "firstName": "Tetsuya"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Tamura",
+          "firstName": "Yoshiaki"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Hasegawa",
+          "firstName": "Takemi"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Taru",
+          "firstName": "Toshiki"
+        }
+      ],
+      "attachments": [],
+      "notes": [],
+      "itemID": 2718,
+      "publicationTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "seeAlso": [],
+      "__citekey__": "hayashi2017"
+    },
+    {
+      "itemType": "conferencePaper",
+      "tags": [
+        "Fiber design and fabrication",
+        "Fiber optics communications"
+      ],
+      "relations": {},
+      "citations": "10",
+      "url": "http://www.osapublishing.org/abstract.cfm?URI=OFC-2016-Th5A.2",
+      "publisher": "Optical Society of America",
+      "pages": "Th5A.2",
+      "date": "2016",
+      "extra": "bibtex: Sakamoto:16",
+      "DOI": "10.1364/OFC.2016.Th5A.2",
+      "abstractNote": "We demonstrate 246 μm cladding 6-mode 19-core fiber and realize twice the spatial density of other multi-core fiber with more than 100 channels, while achieving the lowest loss and differential mode delay of 0.33 ns/km.",
+      "title": "Low-loss and Low-DMD Few-mode Multi-core Fiber with Highest Core Multiplicity Factor",
+      "proceedingsTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "creators": [
+        {
+          "creatorType": "author",
+          "lastName": "Sakamoto",
+          "firstName": "Taiji"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Matsui",
+          "firstName": "Takashi"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Saitoh",
+          "firstName": "Kunimasa"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Saitoh",
+          "firstName": "Shota"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Takenaga",
+          "firstName": "Katsuhiro"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Mizuno",
+          "firstName": "Takayuki"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "abe",
+          "firstName": "yoshiteru"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Shibahara",
+          "firstName": "Kohki"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Tobita",
+          "firstName": "Yuki"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Matsuo",
+          "firstName": "Shoichiro"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Aikawa",
+          "firstName": "Kazuhiko"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Aozasa",
+          "firstName": "Shinichi"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Nakajima",
+          "firstName": "Kazuhide"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Miyamoto",
+          "firstName": "Yutaka"
+        }
+      ],
+      "attachments": [],
+      "notes": [],
+      "itemID": 2719,
+      "publicationTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "seeAlso": [],
+      "__citekey__": "sakamoto2017"
+    },
+    {
+      "itemType": "conferencePaper",
+      "tags": [
+        "Fiber optics and optical communications",
+        "Microstructured fibers"
+      ],
+      "relations": {},
+      "citations": "7",
+      "url": "http://www.osapublishing.org/abstract.cfm?URI=OFC-2016-Th5A.3",
+      "publisher": "Optical Society of America",
+      "pages": "Th5A.3",
+      "date": "2016",
+      "extra": "bibtex: Hayes:16",
+      "DOI": "10.1364/OFC.2016.Th5A.3",
+      "abstractNote": "We report an effectively singlemode tubular antiresonant hollow core fiber with &gt;1000nm bandwidth and record-low loss of 25dB/km, fully connectorizable with SMF28. We demonstrate penalty-free data transmission at wavelengths of 1065, 1565 and 1963nm",
+      "title": "Antiresonant Hollow Core Fiber with Octave Spanning Bandwidth for Short Haul Data Communications",
+      "proceedingsTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "creators": [
+        {
+          "creatorType": "author",
+          "lastName": "Hayes",
+          "firstName": "John R."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Sandoghchi",
+          "firstName": "Seyed R."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Bradley",
+          "firstName": "Thomas D."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Liu",
+          "firstName": "Zhixin"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Slavik",
+          "firstName": "Radan"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Gouveia",
+          "firstName": "Marcelo A."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Wheeler",
+          "firstName": "Natalie V."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Jasion",
+          "firstName": "Gregory T."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Chen",
+          "firstName": "Yong"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Numkam-Fokoua",
+          "firstName": "Eric"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Petrovich",
+          "firstName": "Marco N."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Richardson",
+          "firstName": "David J."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Poletti",
+          "firstName": "Francesco"
+        }
+      ],
+      "attachments": [],
+      "notes": [],
+      "itemID": 2720,
+      "publicationTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "seeAlso": [],
+      "__citekey__": "hayes2017"
+    },
+    {
+      "itemType": "conferencePaper",
+      "tags": [
+        "Fiber characterization",
+        "Fiber optics",
+        "Fiber optics amplifiers and oscillators"
+      ],
+      "relations": {},
+      "citations": "0",
+      "url": "http://www.osapublishing.org/abstract.cfm?URI=OFC-2016-Th5A.4",
+      "publisher": "Optical Society of America",
+      "pages": "Th5A.4",
+      "date": "2016",
+      "extra": "bibtex: Fontaine:16",
+      "DOI": "10.1364/OFC.2016.Th5A.4",
+      "abstractNote": "We demonstrate a multimode cladding-pumped fiber amplifier that supports 10 modes with 2-dB mode-dependent gain, 20-dB gain, and 25-dBm output power.&lt;br /&gt; The mode-dependent gain is minimized using an oversized core supporting additional modes.",
+      "title": "Multi-mode Optical Fiber Amplifier supporting over 10 Spatial Modes",
+      "proceedingsTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "creators": [
+        {
+          "creatorType": "author",
+          "lastName": "Fontaine",
+          "firstName": "Nicolas K."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Huang",
+          "firstName": "Bin"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "sanjabieznaveh",
+          "firstName": "zahoora"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Chen",
+          "firstName": "Haoshuo"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Jin",
+          "firstName": "Cang"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Ercan",
+          "firstName": "Burcu"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Velázquez-Benetez",
+          "firstName": "Amado"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Chang",
+          "firstName": "Sun Hyok"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Ryf",
+          "firstName": "Roland"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Schülzgen",
+          "firstName": "Axel"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Alvarado",
+          "firstName": "Juan Carlos"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Sillard",
+          "firstName": "Pierre"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Gonnet",
+          "firstName": "Cedric"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Antonio-Lopez",
+          "firstName": "Enrique"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Correa",
+          "firstName": "Rodrigo Amezcua"
+        }
+      ],
+      "attachments": [],
+      "notes": [],
+      "itemID": 2721,
+      "publicationTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "seeAlso": [],
+      "__citekey__": "fontaine2017"
+    },
+    {
+      "itemType": "conferencePaper",
+      "tags": [
+        "Fiber devices and optical amplifiers",
+        "Optical vortices"
+      ],
+      "relations": {},
+      "citations": "8",
+      "url": "http://www.osapublishing.org/abstract.cfm?URI=OFC-2016-Th5A.5",
+      "publisher": "Optical Society of America",
+      "pages": "Th5A.5",
+      "date": "2016",
+      "extra": "bibtex: Jung:16",
+      "DOI": "10.1364/OFC.2016.Th5A.5",
+      "abstractNote": "We present for the first time wideband operation of an optical orbital angular momentum amplifier with topological charge textbarltextbar=1. An air-core erbium-doped fiber is fabricated and up to 15.7dB gain is obtained with cladding-pumped configuration.",
+      "title": "Optical Orbital Angular Momentum Amplifier based on an Air-Core Erbium Doped Fiber",
+      "proceedingsTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "creators": [
+        {
+          "creatorType": "author",
+          "lastName": "Jung",
+          "firstName": "Yong-min"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Kang",
+          "firstName": "Qiongyue"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Yoo",
+          "firstName": "Seongwoo"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Raghuraman",
+          "firstName": "Sidharthan"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Ho",
+          "firstName": "Daryl"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Gregg",
+          "firstName": "Patrick"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Ramachandran",
+          "firstName": "Siddharth"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Alam",
+          "firstName": "Shaif-ul"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Richardson",
+          "firstName": "David J."
+        }
+      ],
+      "attachments": [],
+      "notes": [],
+      "itemID": 2722,
+      "publicationTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "seeAlso": [],
+      "__citekey__": "jung2017"
+    },
+    {
+      "itemType": "conferencePaper",
+      "tags": [
+        "Diffraction",
+        "Fiber optics"
+      ],
+      "relations": {},
+      "citations": "3",
+      "url": "http://www.osapublishing.org/abstract.cfm?URI=OFC-2016-Th5A.6",
+      "publisher": "Optical Society of America",
+      "pages": "Th5A.6",
+      "date": "2016",
+      "extra": "bibtex: Fontaine:16",
+      "DOI": "10.1364/OFC.2016.Th5A.6",
+      "abstractNote": "We implement a free-space unitary transform inside a wavelength selective switch to produce rectangular shaped beams on the LCoS steering element. Compared to standard Gaussian illumination, the WSS can support 2${textblackslash}times$ as many ports per pixel.",
+      "title": "Wavelength Selective Switch with Optimal Steering Element Utilization",
+      "proceedingsTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "creators": [
+        {
+          "creatorType": "author",
+          "lastName": "Fontaine",
+          "firstName": "Nicolas K."
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Chen",
+          "firstName": "Haoshuo"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Ercan",
+          "firstName": "Burcu"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Ryf",
+          "firstName": "Roland"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Labroille",
+          "firstName": "Guillaume"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Barre",
+          "firstName": "Nicolas"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Jian",
+          "firstName": "Pu"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Morizur",
+          "firstName": "Jean-Fran¸cois"
+        },
+        {
+          "creatorType": "author",
+          "lastName": "Neilson",
+          "firstName": "David T."
+        }
+      ],
+      "attachments": [],
+      "notes": [],
+      "itemID": 2723,
+      "publicationTitle": "Optical Fiber Communication Conference Postdeadline Papers",
+      "seeAlso": [],
+      "__citekey__": "fontaine2017"
+    }
+  ],
+  "cache": {}
+};
---
+++ src/bubble_chart.js
@@ -0,0 +1,255 @@
+
+
+/* bubbleChart creation function. Returns a function that will
+ * instantiate a new bubble chart given a DOM element to display
+ * it in and a dataset to visualize.
+ *
+ * Organization and style inspired by:
+ * https://bost.ocks.org/mike/chart/
+ *
+ */
+function bubbleChart() {
+  // Constants for sizing
+  var width = 900;
+  var height = 300;
+
+  // tooltip for mouseover functionality
+  var tooltip = floatingTooltip('OFC_tooltip', 240);
+
+
+  // These will be set in create_nodes and create_vis
+  var svg = null;
+  var nodes = [];
+
+  // Nice looking colors - no reason to buck the trend
+  var fillColor = d3.scaleOrdinal()
+    .domain(['Th5A', 'Th5B', 'Th5C'])
+    .range(['#6BC19C', '#CAF185', '#718BBD']);
+
+  var groupsTitleX = {
+    "Th5A": width / 4,
+    'Th5B': 2*(width / 4),
+    'Th5C': 3*(width / 4)
+  };
+  var groupsData = d3.keys(groupsTitleX);
+  
+  // Sizes bubbles based on their area instead of raw radius
+  // var radiusScale = d3.scale.linear()
+  //   .domain([0, 20]).range([10, 200]);
+  var radiusScale = d3.scalePow()
+    .exponent(0.5)
+    .range([2, 85]);
+
+  /*
+   * This data manipulation function takes the raw data from
+   * the CSV file and converts it into an array of node objects.
+   * Each node will store data and visualization values to visualize
+   * a bubble.
+   *
+   * rawData is expected to be an array of data objects, read in from
+   * one of d3's loading functions like d3.csv.
+   *
+   * This function returns the new node array, with a node in that
+   * array for each element in the rawData input.
+   */
+  function createNodes(rawData) {
+    // Use map() to convert raw data into node data.
+    // Checkout http://learnjsdata.com/ for more on
+    // working with data.
+    // var myNodes = rawData.map(function (d) {
+    //   return {
+    //     id: d.id,
+    //     radius: radiusScale(+d.total_amount),
+    //     value: d.total_amount,
+    //     name: d.grant_title,
+    //     org: d.organization,
+    //     group: d.group,
+    //     year: d.start_year,
+    //     x: Math.random() * 900,
+    //     y: Math.random() * 800
+    //   };
+    // });
+    var radiusScale = d3.scalePow()
+        .exponent(0.5)
+        .range([4, 12]);
+    var re = /^([^.]*)/;
+    var myNodes = rawData.map(function (d) {
+      return {
+        id: d.title,
+        radius: radiusScale(+d.citations),
+        value: d.citations,
+        citations: d.citations,
+        name: d.creators[0].firstName +', '+d.creators[0].lastName,
+        org: d.org,
+        key: d.__citekey__,
+        url: d.url,
+        group: re.exec(d.pages)[0],
+        year: d.date,
+        x: Math.random() * 900,
+        y: Math.random() * 800
+      };
+    });
+
+    // sort them to prevent occlusion of smaller nodes.
+    myNodes.sort(function (a, b) { return b.value - a.value; });
+
+    return myNodes;
+  }
+function isolate(force, filter) {
+  var initialize = force.initialize;
+  force.initialize = function() { initialize.call(force, nodes.filter(filter)); };
+  return force;
+}
+  /*
+   * Main entry point to the bubble chart. This function is returned
+   * by the parent closure. It prepares the rawData for visualization
+   * and adds an svg element to the provided selector and starts the
+   * visualization creation process.
+   *
+   * selector is expected to be a DOM element or CSS selector that
+   * points to the parent element of the bubble chart. Inside this
+   * element, the code will add the SVG continer for the visualization.
+   *
+   * rawData is expected to be an array of data objects as provided by
+   * a d3 loading function like d3.csv.
+   */
+  var chart = function chart(selector, rawData) {
+    // Use the max total_amount in the data as the max in the scale's domain
+    // note we have to ensure the total_amount is a number by converting it
+    // with `+`.
+    var maxAmount = d3.max(rawData, function (d) { return +d.total_amount; });
+    radiusScale.domain([0, maxAmount]);
+
+    nodes = createNodes(rawData);
+
+    // Create a SVG element inside the provided selector
+    // with desired size.
+    svg = d3.select(selector)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+    
+    var years = svg.selectAll('.year')
+      .data(groupsData);
+    years.enter().append('text')
+      .attr('class', 'year')
+      .attr('x', function (d) { return groupsTitleX[d]; })
+      .attr('y', 30)
+      .attr('text-anchor', 'middle')
+      .text(function (d) { return d; });
+
+    var simulation = d3.forceSimulation()
+            .force("collide",d3.forceCollide( function(d){return d.radius + 1 }).iterations(30) )
+            .force("charge", d3.forceManyBody())
+            .force("Th5A", isolate(d3.forceX(width / 4), function(d) { return d.group === "Th5A"; }))
+            .force("Th5B", isolate(d3.forceX(2*(width / 4)), function(d) { return d.group === "Th5B"; }))
+            .force("Th5C", isolate(d3.forceX(3*(width / 4)), function(d) { return d.group === "Th5C"; }))
+            //.force("center", d3.forceCenter(width / 2, height / 2))
+            .force("y", d3.forceY(height / 2))
+            // .force("y", d3.forceY(0))
+            //.force("x", d3.forceX(0))
+    
+    var node = svg.selectAll("circle")
+      .data(nodes, function(d) { return d.id;})
+      .enter()
+      .append('g').classed('node', true);
+    node.append("a")
+      .attr("xlink:target", function(d) {return "_top"})
+      .attr("xlink:href", function(d) {return d.url})
+      .append('circle')
+      .classed('bubble', true)
+      .attr('r', function(d){  return d.radius })
+      .attr('fill', function (d) { return fillColor(d.group); })
+      .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
+      .attr('stroke-width', 2)
+      .on('mouseover', showDetail)
+      .on('mouseout', hideDetail);;
+    // node.append("title")
+    //   .text(function(d) { return '# cites: ' + d.citations.toString() ; });
+    node.append("text")
+      .attr("dy", 0)
+      .attr("dx", -4)
+      .attr('fill',"black")
+      .text(function(d) { return d.citations; });
+    
+    var ticked = function() {
+      node.attr("transform", function(d) { return "translate(" + d.x + ", " + d.y + ")"; });
+    // node.attr("cx", function(d) { return d.x; })
+    //     .attr("cy", function(d) { return d.y; });
+    }
+    
+    simulation.nodes(nodes, function(d) { return d.id;})
+              .on("tick", ticked);
+  };
+
+  /*
+   * Function called on mouseover to display the
+   * details of a bubble in the tooltip.
+   */
+  function showDetail(d) {
+    // change outline to indicate hover state.
+    d3.select(this).attr('stroke', 'black');
+
+    var content = '<span class="name">Title: </span><span class="value">' +
+                  d.id +
+                  '</span><br/>' +
+                  '<span class="name">Author: </span><span class="value">' +
+                  d.name +
+                   '</span><br/>' +
+                   '<span class="name">Organization: </span><span class="value">' +
+                  d.org +
+                  '</span><br/>' +
+                  '<span class="name">Citations: </span><span class="value">' +
+                  d.value +
+                  '</span><br/>' +
+                  '<span class="name">Group: </span><span class="value">' +
+                  d.group +
+                  '</span><br/>' +
+                  '<span class="name">Year: </span><span class="value">' +
+                  d.year +
+                  '</span>';
+    tooltip.showTooltip(content, d3.event);
+    // d3.select("#tooltip").transition()
+    //      .duration(200)
+    //      .style("opacity", .9);
+    d3.select("#tooltip")   
+         .style("left", (d3.event.pageX) + "px")         
+         .style("top", (d3.event.pageY - 28) + "px");
+  };
+
+  /*
+   * Hides tooltip
+   */
+  function hideDetail(d) {
+    // reset outline
+    d3.select(this)
+      .attr('stroke', d3.rgb(fillColor(d.group)).darker());
+
+    tooltip.hideTooltip();
+  }
+  // return the chart function from closure.
+  return chart;
+}
+
+/*
+ * Below is the initialization code as well as some helper functions
+ * to create a new bubble chart instance, load the data, and display it.
+ */
+
+var myBubbleChart = bubbleChart();
+
+/*
+ * Function called once data is loaded from CSV.
+ * Calls bubble chart function to display inside #vis div.
+ */
+function display(error, data) {
+  if (error) {
+    console.log(error);
+  }
+  // console.log(data[0])
+  myBubbleChart('#vis', data);
+}
+
+// Load the data.
+var data = d3.json('data/OFC-2016-Th5.json', display);
+
